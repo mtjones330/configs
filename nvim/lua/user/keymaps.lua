735ea9eb9f65957117e45b2b5d849f2eef76a06e
Matthew Jones <matthew.jones@dcsg.com>
2022-07-05 18:06:33 -0400

    Update

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -58,10 +58,10 @@ keymap("v", ">", ">gv", opts)
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
-keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
-keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "ff", ":Telescope find_files<CR>", opts)
+keymap("n", "fg", ":Telescope live_grep<CR>", opts)
+keymap("n", "fp", ":Telescope projects<CR>", opts)
+keymap("n", "fb", ":Telescope buffers<CR>", opts)
 
 -- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
