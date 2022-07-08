7af216b97ee0430143f75cee3653b9b676a2bdd5
Matthew Jones <14156163+mtjones330@users.noreply.github.com>
2022-07-08 18:23:10 -0400

    Update

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -9,16 +9,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- Remove statusline and tabline when in Alpha
---vim.api.nvim_create_autocmd({ "User" }, {
---  pattern = { "AlphaReady" },
---  callback = function()
---    vim.cmd [[
---      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
---      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
---    ]]
---  end,
---})
+-- Remove tabline when in Alpha
+vim.api.nvim_create_autocmd({ "User" }, {
+ pattern = { "AlphaReady" },
+ callback = function()
+   vim.cmd [[
+     set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+   ]]
+ end,
+})
 
 -- Set wrap and spell in markdown and gitcommit
 vim.api.nvim_create_autocmd({ "FileType" }, {
@@ -44,3 +43,4 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
   end,
 })
+
