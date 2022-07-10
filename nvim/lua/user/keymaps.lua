f3539724e2a3ceef35dc709cbe4ad0be66db781b
Matthew Jones <14156163+mtjones330@users.noreply.github.com>
2022-07-09 20:43:04 -0400

    Update

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -23,25 +23,25 @@ vim.g.mapleader = " "
 keymap("n", "<leader>v", "<C-w>v", opts)
 keymap("n", "<leader>s", "<C-w>s", opts)
 
-keymap("n", "<leader>h", "<C-w>h", opts)
-keymap("n", "<leader>j", "<C-w>j", opts)
-keymap("n", "<leader>k", "<C-w>k", opts)
-keymap("n", "<leader>l", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
---keymap("n", "<c-Up>", ":resize -2<cr>", opts)
---keymap("n", "<C-Down>", ":resize +2<CR>", opts)
---keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
---keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<cr>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<C-l>", ":BufferLineMoveNext<CR>", opts)
-keymap("n", "<C-h>", ":BufferLineMovePrev<CR>", opts)
+keymap("n", "<A-l>", ":BufferLineMoveNext<CR>", opts)
+keymap("n", "<A-h>", ":BufferLineMovePrev<CR>", opts)
 
 -- Clear highlights
-keymap("n", "<leader>c", "<cmd>nohlsearch<CR>", opts)
+keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 
 -- Close buffers
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
@@ -63,7 +63,7 @@ keymap("v", "p", '"_dP', opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
-
+-- Move text up and down  
 keymap("v", "K", ":m '<-2<CR>gv=gv")
 keymap("v", "J", ":m '>+1<CR>gv=gv")
 
