5d6c629488cb2068c66e706ee196f73d5cbfb1a3
mtjones330 <14156163+mtjones330@users.noreply.github.com>
2022-07-20 22:29:31 -0400

    Update

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -27,6 +27,7 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<leader>q", "<cmd>q<CR>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<cr>", opts)
