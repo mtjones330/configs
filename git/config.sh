c0ddadfbf855ab8ddf4b816b467971d866b10756
mtjones330 <14156163+mtjones330@users.noreply.github.com>
2022-07-20 22:39:41 -0400

    Update

---
+++ git/config.sh
@@ -0,0 +1,6 @@
+git config --global user.name mtjones330
+git config --global user.email 14156163+mtjones330@users.noreply.github.com
+git config --global pull.rebase true
+git config --global fetch.prune true
+git config --global diff.colorMoved zebra
+git config --global core.editor nvim
