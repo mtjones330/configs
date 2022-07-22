e938cdc555c2d7c0d8442dbafa2e68a314fa0079
Matthew Jones <matthew.jones@dcsg.com>
2022-07-22 14:40:10 -0400

    Update

--- git/config.sh
+++ git/config.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 git config --global user.name mtjones330
 git config --global user.email 14156163+mtjones330@users.noreply.github.com
 git config --global pull.rebase true
 git config --global fetch.prune true
 git config --global diff.colorMoved zebra
-git config --global core.editor nvim
+git config --global diff.tool nvimdiff
+git config --global merge.tool nvimdiff
+git config --global --add difftool.prompt false
