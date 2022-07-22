4bd2b1f22e2f31846197f2909c38b359ac97783b
Matthew Jones <matthew.jones@dcsg.com>
2022-07-22 14:08:50 -0400

    Add lualine

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -9,14 +9,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- Remove tabline when in Alpha
+-- Remove statusline and tabline when in Alpha
 vim.api.nvim_create_autocmd({ "User" }, {
- pattern = { "AlphaReady" },
- callback = function()
-   vim.cmd [[
-     set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-   ]]
- end,
+  pattern = { "AlphaReady" },
+  callback = function()
+    vim.cmd [[
+      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
+    ]]
+  end,
 })
 
 -- Set wrap and spell in markdown and gitcommit
