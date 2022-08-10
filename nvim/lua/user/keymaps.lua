1dd757a2903040d66ddfe6ccef9e9cdbb507929b
mtjones330 <14156163+mtjones330@users.noreply.github.com>
2022-08-09 23:32:26 -0400

    Change move buffer to greater than and less than

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -38,8 +38,8 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<A-l>", ":BufferLineMoveNext<CR>", opts)
-keymap("n", "<A-h>", ":BufferLineMovePrev<CR>", opts)
+keymap("n", "<", ":BufferLineMovePrev<CR>", opts)
+keymap("n", ">", ":BufferLineMoveNext<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
